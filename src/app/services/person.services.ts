574bc18e853fd50b7485cc8db496470e95242bba
jmukhadze <119632994+jmukhadze@users.noreply.github.com>
2022-12-06 20:05:55 +0400

    Add files via upload

---
+++ src/app/services/person.services.ts
@@ -0,0 +1,15 @@
+import{Observable, of} from "rxjs";
+import {Injectable} from "@angular/core";
+
+
+@Injectable({
+  providedIn:'root'
+})
+
+export class TodoService {
+  get  todos(): ITodo{
+
+  }
+  constructor() {
+  }
+}
